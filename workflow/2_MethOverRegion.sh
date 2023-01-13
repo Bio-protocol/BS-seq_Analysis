11b1d06a4cedb3e68b27c204728e15bf00bc795e
Jing Xu <108569109+Baleencanfly@users.noreply.github.com>
2023-01-13 16:47:15 +0800

    Update 2_MethOverRegion.sh

--- workflow/2_MethOverRegion.sh
+++ workflow/2_MethOverRegion.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+##usage: bash file1
 
-awk '{if($1!="chr") print $1"\t"$2"\t"$3"\t"$7"\t"$8-$7"\t"$4"\t"$4}' /path/to/aligntoREF/BSMAPratio/SampleX > /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab
+SampleX=$1
 
-bgzip /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab
+awk '{if($1!="chr") print $1"\t"$2"\t"$3"\t"$7"\t"$8-$7"\t"$4"\t"$4}' /path/to/aligntoREF/BSMAPratio/${SampleX} > /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab
 
-tabix -C -p vcf /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab.gz
+bgzip /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab
 
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab.gz,"SampleX_CG" --region /path/to/REFfile/target_gene.bed --prefix SampleX_target_gene --context CG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+tabix -C -p vcf /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz
 
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab.gz,"SampleX_CHG" --region /path/to/REFfile/target_gene.bed --prefix SampleX_target_gene --context CHG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CG" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
 
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/SampleX_forViewBS.tab.gz,"SampleX_CHH" --region /path/to/REFfile/target_gene.bed --prefix SampleX_target_gene --context CHH --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CHG" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CHG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+
+ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CHH" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CHH --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
