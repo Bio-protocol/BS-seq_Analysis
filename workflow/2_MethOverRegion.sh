66aab38fdf9f0006615c30f28f007d1a0b19798b
Jing Xu <108569109+Baleencanfly@users.noreply.github.com>
2023-04-14 18:36:18 +0800

    Update 2_MethOverRegion.sh

--- workflow/2_MethOverRegion.sh
+++ workflow/2_MethOverRegion.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-##usage: bash file1
+##usage: bash file1 file2
 
 SampleX=$1
-
-##Transfer the data format for ViewBS
-awk '{if($1!="chr") print $1"\t"$2"\t"$3"\t"$7"\t"$8-$7"\t"$4"\t"$4}' /path/to/aligntoREF/BSMAPratio/${SampleX} > /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab
+Region=$2
 
 ##ViewBS requires input of compressed file format
-bgzip /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab
+bgzip ${SampleX}_forViewBS.tab
 
-tabix -C -p vcf /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz
+tabix -C -p vcf ${SampleX}_forViewBS.tab.gz
 
 ##This step can calculate DNA methylation average level in target region. The value is output in the txt file, and the picture is output in the pdf file.
 ##The target genes here including total genes in maize.
 ##Users can modify the figure using value output by 3_Metagene_plot.R
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CG" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+ViewBS MethOverRegion --sample ${SampleX}_forViewBS.tab.gz,"CG" --region ${Region} --prefix ${SampleX}_target_gene --context CG --outdir ./MethOverRegion --binNumber 20 --binLength 100
 
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CHG" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CHG --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+ViewBS MethOverRegion --sample ${SampleX}_forViewBS.tab.gz,"CHG" --region ${Region} --prefix ${SampleX}_target_gene --context CHG --outdir ./MethOverRegion --binNumber 20 --binLength 100
 
-ViewBS MethOverRegion --sample /path/to/aligntoREF/ViewBS/${SampleX}_forViewBS.tab.gz,"CHH" --region /path/to/REFfile/target_gene.bed --prefix ${SampleX}_target_gene --context CHH --outdir /path/to/aligntoREF/ViewBS/MethOverRegion --binNumber 20 --binLength 100
+ViewBS MethOverRegion --sample ${SampleX}_forViewBS.tab.gz,"CHH" --region ${Region} --prefix ${SampleX}_target_gene --context CHH --outdir ./MethOverRegion --binNumber 20 --binLength 100
